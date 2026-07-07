b9331678b59e454bfde96eb6c7c25da834300c9a
Cleo Aguiar <cleotaisas@gmail.com>
2026-07-07 20:39:16 -0300

    feat: add swipe gesture controls for touch devices

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,37 @@
 extends RigidBody2D
 
 var force = 100
+var swipe_impulse := 25
+
+var touch_start := Vector2.ZERO
+var swipe_threshold := 50
+
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("move_right"):
-		apply_force(Vector2(force,0))
+		apply_force(Vector2.RIGHT * force)
 	if Input.is_action_pressed("move_left"):
-		apply_force(Vector2(-force,0))
+		apply_force(Vector2.LEFT * force)
 	if Input.is_action_pressed("move_up"):
-		apply_force(Vector2(0, -force))
+		apply_force(Vector2.UP * force)
 	if Input.is_action_pressed("move_down"):
-		apply_force(Vector2(0, force))
+		apply_force(Vector2.DOWN * force)
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			touch_start = event.position
+		else:
+			var delta: Vector2 = event.position - touch_start
+			if delta.length() < swipe_threshold:
+				return
+			if abs(delta.x) > abs(delta.y):
+				if delta.x > 0:
+					apply_impulse(Vector2.RIGHT * swipe_impulse)
+				else:
+					apply_impulse(Vector2.LEFT * swipe_impulse)
+			else:
+				if delta.y > 0:
+					apply_impulse(Vector2.DOWN * swipe_impulse)
+				else:
+					apply_impulse(Vector2.UP * swipe_impulse)
