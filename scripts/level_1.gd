bac09d32845c3a0cf3d3b121e14c61ff00e76f93
Cleo Aguiar <cleotaisas@gmail.com>
2026-07-16 21:27:12 -0300

    style: add stylized titles. Close #1

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_maze_body_exited(body: Node2D) -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
