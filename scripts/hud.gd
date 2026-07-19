0650e4211977979f53c82dbb4405e96919d75d21
Cleo Aguiar <cleotaisas@gmail.com>
2026-07-18 23:02:04 -0300

    feat: add a stopwatch to measure the race time

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,5 +1,13 @@
 extends Control
 
+@onready var circuit_label: Label = $MarginContainer/HBoxContainer/LeftBoxContainer/TitleContainer/CircuitLabel
+@onready var timer_label: Label = $MarginContainer/HBoxContainer/LeftBoxContainer/TimerLabel
+
+func set_circuit_label(circuit: String) -> void:
+	circuit_label.text = circuit
+
+func update_timer_display(time_left: String) -> void:
+	timer_label.text = str(time_left)
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
