340724ad4c5473635c13c3fe3d48549c8939c42f
Cleo Aguiar <cleotaisas@gmail.com>
2026-08-02 22:45:44 -0300

    fix(audio): add missing button sound effects in settings

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,5 @@
 extends Control
 
-@onready var play_button: Button = $VBoxContainer/PlayButton
-@onready var credits_button: Button = $VBoxContainer/CreditsButton
-@onready var quit_button: Button = $VBoxContainer/QuitButton
-
 func _ready():
 	get_tree().paused = false
 
@@ -14,10 +10,6 @@ func _on_play_button_pressed() -> void:
 func _on_settings_button_pressed() -> void:
 	AudioManager.play_button()
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
-
-func _on_credits_button_pressed() -> void:
-	AudioManager.play_button()
-	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 func _on_quit_button_pressed() -> void:
 	AudioManager.play_button()
